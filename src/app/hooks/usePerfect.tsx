28e4586b6874ea27e97630e9f79c043ae7499293
Miles Wiesenthal <mileswiesenthal@gmail.com>
2024-09-17 07:18:27 -0700

    state nonsense

---
+++ src/app/hooks/usePerfect.tsx
@@ -0,0 +1,107 @@
+import { useState, useRef, useEffect, MutableRefObject } from "react";
+
+const isFunction = <T,>(
+  value: T | ((prevState: T) => T),
+): value is (prevState: T) => T => typeof value === "function";
+
+export const f =
+  <T, R = T>(...args: T[]) =>
+  (func: (...args: T[]) => R) =>
+    func(...args);
+
+export const to = <T,>(arg: T) => arg;
+
+export const first =
+  <T,>(func: (...args: T[]) => void): ((...args: T[]) => void) =>
+  (arg: T) =>
+    func(arg);
+
+export const eq =
+  <T,>(x: T) =>
+  (y: T) =>
+    x === y;
+
+const ne =
+  <T,>(x: T) =>
+  (y: T) =>
+    y !== x;
+
+type v<T> = (value: T) => void;
+type b<T> = (value: T) => boolean;
+
+const expect =
+  <T,>(predicate: b<T>) =>
+  (push: v<v<T>>) =>
+  (resolve: v<boolean>) =>
+    push((v) => predicate(v) && (resolve(true), true));
+
+const neg =
+  <T,>(predicate: b<T>) =>
+  (v: T) =>
+    !predicate(v);
+
+const p =
+  <T,>(ref: MutableRefObject<v<T>[]>): v<v<T>> =>
+  (resolve: v<T>) => (ref.current.push(resolve), undefined);
+
+export default function usePerfect<T>(initialState: T | (() => T)) {
+  const [state, _setState] = useState(initialState);
+  const laggingRef = useRef<T | null>(null);
+  const ref = useRef(state);
+  const resolves = useRef<v<T>[]>([]);
+  const isResolves = useRef<b<T>[]>([]);
+
+  const transitionCallbacks = useRef<
+    ((nextState: T, previousState?: T | null) => void)[]
+  >([]);
+
+  const setState = useRef((newState: React.SetStateAction<T>) => {
+    const previousState = ref.current;
+    const nextState = isFunction(newState) ? newState(ref.current) : newState;
+    if (previousState === nextState) return;
+    _setState(nextState);
+    ref.current = nextState;
+    transitionCallbacks.current.forEach(f(nextState, previousState));
+    return nextState;
+  }).current;
+
+  const onTransition = useRef(
+    (callback: (nextState: T, previousState?: T | null) => void) => (
+      p(transitionCallbacks)(callback),
+      () => (
+        (transitionCallbacks.current = transitionCallbacks.current.filter(
+          ne(callback),
+        )),
+        undefined
+      )
+    ),
+  ).current;
+
+  useEffect(() => {
+    laggingRef.current = state;
+    resolves.current.forEach(f(state));
+    resolves.current.splice(0);
+    isResolves.current.forEach(f(state));
+    isResolves.current = isResolves.current.filter(neg(f(state)));
+  }, [state]);
+
+  const resolve = useRef(async () =>
+    laggingRef.current === ref.current
+      ? ref.current
+      : new Promise<T>(p(resolves)),
+  ).current;
+
+  const is = useRef(
+    async (predicate: (state: T) => boolean) =>
+      new Promise<boolean>(expect(predicate)(p(isResolves))),
+  ).current;
+
+  return {
+    ref,
+    state,
+    setState,
+    onTransition,
+    resolve,
+    is,
+  };
+}
---
+++ src/app/ridiculous/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useEffect } from "react";
+import usePerfect, { eq } from "../hooks/usePerfect";
+
+export default function HomePage() {
+  const { ref, state, setState, onTransition, resolve, is } = usePerfect(0);
+  const { state: debug, setState: setDebug } = usePerfect("");
+
+  useEffect(() => {
+    return onTransition((nextState, previousState) =>
+      setDebug(`${previousState} -> ${nextState}`),
+    );
+  }, []);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between bg-gradient-to-br from-[#d1c4ff] via-zinc-100 to-[#a0ffde] text-black">
+      <div className="container flex h-2/3 flex-col items-center justify-center gap-4 space-y-8 p-4 md:p-16">
+        <button
+          onClick={async () => {
+            setState(state + 1);
+            const x = await resolve();
+            console.log("x", x, state);
+          }}
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => {
+            setState(state - 1);
+          }}
+        >
+          Decrement
+        </button>
+        <button
+          onClick={async () => {
+            const awaitedState = await is(eq(5)).catch((e) => {
+              console.error("e", e);
+            });
+            setDebug(JSON.stringify({ awaitedState, state }, null, 2));
+          }}
+        >
+          Assert
+        </button>
+        <div>state: {state}</div>
+        <pre>{debug}</pre>
+      </div>
+    </main>
+  );
+}
