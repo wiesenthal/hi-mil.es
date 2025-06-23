05c932b9a679ab8edd47b6e280b9b933366a40bb
Miles Wiesenthal <mileswiesenthal@gmail.com>
2025-06-23 12:20:25 -0700

    increase symbol padding

--- src/app/components/Symbol.tsx
+++ src/app/components/Symbol.tsx
@@ -7,7 +7,7 @@ export const Symbol = ({
 }) =>
   children ? (
     <div
-      className={`absolute -right-6 top-[50%] -translate-y-1/2 ${className}`}
+      className={`absolute -right-7 top-[50%] -translate-y-1/2 ${className}`}
     >
       {children}
     </div>
